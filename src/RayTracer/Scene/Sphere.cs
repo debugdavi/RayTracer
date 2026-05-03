e96be9d052e0fa56731e9d8d8a5c270dbd58d2cd
Davi Lucas <davilucasgaraujo2@gmail.com>
2026-05-03 16:37:54 -0300

    feat: Adiciona classe Sphere com detecção de interseção
    Implementa a classe Sphere no namespace RayTracer.Scene, incluindo propriedades para centro e raio, além do método Hit para calcular a interseção de um raio com a esfera usando a equação quadrática. O método retorna se houve interseção e a distância até o ponto de impacto.

---
+++ src/RayTracer/Scene/Sphere.cs
@@ -0,0 +1,39 @@
+﻿using RayTracer.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RayTracer.Scene
+{
+    public class Sphere
+    {
+        public Vector3 Center { get; set; }
+        public double Radius;
+        public Sphere(Vector3 center, double radius)
+        {
+            Center = center;
+            Radius = radius;
+        }
+        public bool Hit(Ray ray, out double t)
+        {
+            Vector3 oc = ray.Origin - Center;
+
+            double a = Vector3.DotProduct(ray.Direction, ray.Direction);
+            double b = 2.0 * Vector3.DotProduct(oc, ray.Direction);
+            double c = Vector3.DotProduct(oc, oc) - Radius * Radius;
+
+            double discriminant = b * b - 4 * a * c;
+
+            if (discriminant < 0)
+            {
+                t = 0;
+                return false;
+            }
+
+            t = (-b - Math.Sqrt(discriminant)) / (2.0 * a);
+            return true;
+        }
+    }
+}
