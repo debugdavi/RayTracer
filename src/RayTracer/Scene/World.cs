3592786bbc9166f768345987359d778875796d46
Davi Lucas <davilucasgaraujo2@gmail.com>
2026-05-06 20:44:11 -0300

    feat:
    Adiciona classe World para gerenciamento de esferas e traçado
    
    Implementa a classe World no namespace RayTracer.Scene, permitindo adicionar esferas e realizar o traçado de raios para detectar interseções. O método Trace retorna a cor baseada na normal do ponto de impacto ou uma cor de fundo interpolada caso não haja colisão.

---
+++ src/RayTracer/Scene/World.cs
@@ -0,0 +1,45 @@
+﻿using RayTracer.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RayTracer.Scene
+{
+    public class World
+    {
+        private List<Sphere> _spheres = new List<Sphere>();
+
+        public void Add(Sphere sphere) => _spheres.Add(sphere);
+
+        public Vector3 Trace(Ray ray)
+        {
+            double closestT = double.MaxValue;
+            Sphere hitObject = null;
+
+            foreach (var sphere in _spheres)
+            {
+                if (sphere.Hit(ray, out double t))
+                {
+                    if (t > 0.001 && t < closestT)
+                    {
+                        closestT = t;
+                        hitObject = sphere;
+                    }
+                }
+            }
+
+            if (hitObject != null)
+            {
+                Vector3 point = ray.At(closestT);
+                Vector3 normal = (point - hitObject.Center).Normalize();
+                return 0.5 * (normal + new Vector3(1, 1, 1));
+            }
+
+            // Fundo (Céu)
+            double tBg = 0.5 * (ray.Direction.Normalize().Y + 1.0);
+            return (1.0 - tBg) * new Vector3(1, 1, 1) + tBg * new Vector3(0.5, 0.7, 1.0);
+        }
+    }
+}
