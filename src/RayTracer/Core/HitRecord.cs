d92fccbd31d22350359d24c6e32d8bc4bf9ea277
Davi Lucas <davilucasgaraujo2@gmail.com>
2026-06-16 23:25:32 -0300

    feat: Cria classe `HitRecord` Contém propriedades para armazenar informações de interseção em um ray tracer (`T`, `Point`, `Normal` e `Material`).

---
+++ src/RayTracer/Core/HitRecord.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RayTracer.Core
+{
+    public class HitRecord
+    {
+        public double T { get; set; }
+        public Vector3 Point { get; set; }
+        public Vector3 Normal { get; set; }
+        public Material Material { get; set; }
+    }
+}
