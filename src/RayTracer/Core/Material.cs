456ced773c96971012cdeff18e5612bbbda0d87c
Davi Lucas <davilucasgaraujo2@gmail.com>
2026-06-16 23:25:56 -0300

    feat: Adiciona classe Material para representar propriedades ópticas.

---
+++ src/RayTracer/Core/Material.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RayTracer.Core
+{
+    public class Material
+    {
+        public Vector3 Color { get; set; }
+        public double Ka { get; set; }
+        public double Kd { get; set; }
+        public double Ks { get; set; }
+        public double Shininess { get; set; }
+
+        public Material(Vector3 color, double ka, double kd, double ks, double shininess)
+        {
+            Color = color;
+            Ka = ka;
+            Kd = kd;
+            Ks = ks;
+            Shininess = shininess;
+        }
+    }
+}
