233be551d4738658f8fe763fb5f0aac6c4b056a6
Davi Lucas <davilucasgaraujo2@gmail.com>
2026-07-21 21:36:20 -0300

    feat: Adiciona materiais espelhado e vidro à cena

--- src/RayTracer/Program.cs
+++ src/RayTracer/Program.cs
@@ -28,13 +28,23 @@ Texture texTerra = new Texture("Textures/earth.jpg");
 Material matChao = new Material(texChao, ka: 0.2, kd: 0.8, ks: 0.1, shininess: 8);
 Material matTerra = new Material(texTerra, ka: 0.1, kd: 0.8, ks: 0.4, shininess: 32);
 Material matCaixa = new Material(texCaixa, ka: 0.1, kd: 0.8, ks: 0.2, shininess: 16);
-Material matPolido = new Material(new Vector3(0.9, 0.1, 0.1), ka: 0.1, kd: 0.7, ks: 0.9, shininess: 128); // Sólido e muito brilhante
+Material matPolido = new Material(new Vector3(0.9, 0.1, 0.1), ka: 0.1, kd: 0.7, ks: 0.9, shininess: 128);
+
+// NOVO: Material espelhado (reflexão)
+Material matEspelho = new Material(new Vector3(0.95, 0.95, 0.95), ka: 0.02, kd: 0.05, ks: 0.9, shininess: 200)
+    { Reflectivity = 0.85 };
+
+// NOVO: Material vidro (refração + reflexão via Fresnel)
+Material matVidro = new Material(new Vector3(0.95, 0.95, 1.0), ka: 0.0, kd: 0.02, ks: 0.9, shininess: 200)
+    { Transparency = 0.9, RefractiveIndex = 1.5 };
 
 // ===== Geometria =====
 Mesh chao = ObjLoader.Load("Models/plane.obj", matChao);
 Mesh caixa = ObjLoader.Load("Models/cube.obj", matCaixa);
 Mesh teapot = ObjLoader.Load("Models/teapot.obj", matPolido);
 Sphere terra = new Sphere(new Vector3(0, 0, 0), 1.0, matTerra);
+Sphere espelho = new Sphere(new Vector3(0, 0, 0), 1.0, matEspelho);
+Sphere vidro = new Sphere(new Vector3(0, 0, 0), 1.0, matVidro);
 
 // ===== Hierarquia de Cena =====
 SceneNode raiz = new SceneNode();
@@ -42,16 +52,24 @@ SceneNode raiz = new SceneNode();
 // 1. Chão (Escalado para ser bem grande)
 raiz.AddChild(new SceneNode(Transform.Scale(4), chao));
 
-// 2. Terra (No meio, um pouco para trás e levantada)
-Matrix4x4 terraTransform = Transform.Translate(0, 1.0, -1) * Transform.RotateY(-45);
+// 2. Terra (Centro-esquerda)
+Matrix4x4 terraTransform = Transform.Translate(-1.5, 1.0, -1) * Transform.RotateY(-45);
 raiz.AddChild(new SceneNode(terraTransform, terra));
 
-// 3. Caixa (Na esquerda, rotacionada pra dar um charme)
-Matrix4x4 caixaTransform = Transform.Translate(-2.0, 1.0, 0.5) * Transform.RotateY(30) * Transform.RotateX(15);
+// 3. Esfera Espelhada (Centro)
+Matrix4x4 espelhoTransform = Transform.Translate(1.0, 0.8, 1.0) * Transform.Scale(0.8);
+raiz.AddChild(new SceneNode(espelhoTransform, espelho));
+
+// 4. Esfera de Vidro (Centro-direita, menor)
+Matrix4x4 vidroTransform = Transform.Translate(-0.3, 0.5, 2.0) * Transform.Scale(0.5);
+raiz.AddChild(new SceneNode(vidroTransform, vidro));
+
+// 5. Caixa (Fundo esquerdo)
+Matrix4x4 caixaTransform = Transform.Translate(-3.0, 1.0, -2) * Transform.RotateY(30) * Transform.RotateX(15);
 raiz.AddChild(new SceneNode(caixaTransform, caixa));
 
-// 4. Teapot (Na direita)
-Matrix4x4 teapotTransform = Transform.Translate(2.0, 0.0, 0.5) * Transform.RotateY(-30) * Transform.Scale(0.7);
+// 6. Teapot (Fundo direito)
+Matrix4x4 teapotTransform = Transform.Translate(3.0, 0.0, -1) * Transform.RotateY(-30) * Transform.Scale(0.5);
 raiz.AddChild(new SceneNode(teapotTransform, teapot));
 
 // ===== Aplanar Hierarquia =====
